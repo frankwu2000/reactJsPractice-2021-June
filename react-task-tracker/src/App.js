9904de7f0033673cdf2ae19539a646be7823a2af
hongyi wu <frankwu2000@hotmail.com>
2021-06-06 14:48:30 -0700

    ReactTrackerApp addform step

---
+++ react-task-tracker/src/App.js
@@ -0,0 +1,53 @@
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import { useState } from "react";
+
+const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Food Shopping",
+      day: "Feb 5th at 2:30 pm",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Drink Shopping",
+      day: "Feb 6th at 2:30 pm",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Game Shopping",
+      day: "Feb 7th at 2:30 pm",
+      reminder: false,
+    },
+  ]);
+
+  // Delete task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  // Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
+  return (
+    <div className="container">
+      <Header title="Task Tracker" />
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks To Show"
+      )}
+    </div>
+  );
+};
+
+export default App;
