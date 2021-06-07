f91b47eacbbb369665fbb40d8e3972610e4c88af
hongyi wu <hongyiwu@hongyis-MacBook-Pro-2.local>
2021-06-06 21:42:18 -0700

    complete taskTrack with jsonServer

--- react-task-tracker/src/components/Header.js
+++ react-task-tracker/src/components/Header.js
@@ -1,15 +1,24 @@
 import PropTypes from "prop-types";
+import { useLocation } from "react-router-dom";
 import Button from "./Button";
 
-const Header = ({ title }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   const onClick = (e) => {
-    console.log("click");
+    onAdd();
   };
+
+  const location = useLocation();
 
   return (
     <header className="header">
       <h1>{title}</h1>
-      <Button color="green" text="Add" onClick={onClick} />
+      {location.pathname === "/" && (
+        <Button
+          color={showAdd ? "red" : "green"}
+          text={showAdd ? "Close" : "Add"}
+          onClick={onClick}
+        />
+      )}
     </header>
   );
 };
